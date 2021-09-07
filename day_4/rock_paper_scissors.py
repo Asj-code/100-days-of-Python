5ef12847956ccbdc08f5e92bbbf6966e9094e08d
Andrea Juarez <Andre@Andreas-MacBook-Air.local>
2021-09-07 19:27:38 -0300

    segundo commit

---
+++ day_4/rock_paper_scissors.py
@@ -0,0 +1,55 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+options = [rock, paper, scissors]
+
+selection = int(input("Type 0 for rock, 1 for paper and 2 for scissors: "))
+print("You have selected: " + options[selection])
+
+computer_selection = random.randint(0, 2)
+print("The computer has selected: " + options[computer_selection])
+
+if selection == 0 and computer_selection == 0:
+  print("It's a draw!")
+elif selection == 0 and computer_selection == 1:
+  print("You lose!")
+elif selection == 0 and computer_selection == 2:
+  print("You win")
+elif selection == 1 and computer_selection == 0:
+  print("You win")
+elif selection == 1 and computer_selection == 1:
+  print("It's a draw!")
+elif selection == 1 and computer_selection == 2:
+  print("You lose")
+elif selection == 2 and computer_selection == 0:
+  print("You lose")
+elif selection == 2 and computer_selection == 1:
+  print("You win")
+else:
+  print("It's a draw!")
